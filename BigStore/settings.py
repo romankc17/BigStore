70e9de2f07a5af6344bb278a60933d03d5e9eecc
romankc17 <romanchhetri02@gmail.com>
2021-07-16 13:49:45 +0545

    project directory structures updated

--- BigStore/settings.py
+++ BigStore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = [
+    
+    'http://localhost:3000'
+    
+]
